57f74a7b5231b9ab6365c95e96859cbf23bd6c10
Suyash Gupta <suyashgupta2411@gmail.com>
2026-01-13 17:51:15 +0530

    final hospital staffing system with secure config and tests

--- backend/homework/5/src/test/java/com/smartcity/hospital/HospitalApplicationTests.java
+++ backend/homework/5/src/test/java/com/smartcity/hospital/HospitalApplicationTests.java
@@ -1,13 +1,16 @@
 package com.smartcity.hospital;
 
+import com.smartcity.hospital.config.JdbcConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JdbcConfig.class))
 class HospitalApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
