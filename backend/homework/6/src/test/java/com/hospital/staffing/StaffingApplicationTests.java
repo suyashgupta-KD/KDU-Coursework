7555767f04f1e115413914ba670c88aa09abb1b9
Suyash Gupta <suyashgupta2411@gmail.com>
2026-01-14 14:24:43 +0530

    configure H2 in-memory database for application context tests

--- backend/homework/6/src/test/java/com/hospital/staffing/StaffingApplicationTests.java
+++ backend/homework/6/src/test/java/com/hospital/staffing/StaffingApplicationTests.java
@@ -2,12 +2,13 @@ package com.hospital.staffing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StaffingApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
