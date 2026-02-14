c2767fd35f930d01ef73f6cd56c51f2fb78b4f44
Suyash Gupta <suyashgupta2411@gmail.com>
2026-02-15 03:26:40 +0530

    Personal Details state slice - complete

--- frontend/assignments/2/src/app/store.ts
+++ frontend/assignments/2/src/app/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { bookingConfigApi } from "../features/bookingConfig/bookingConfigApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import bookingTimelineReducer from "../features/bookingTimeline/bookingTimelineSlice";
+import personalDetailsReducer from "../features/personalDetails/personalDetailsSlice";
 export const store = configureStore({
   reducer: {
     [bookingConfigApi.reducerPath]: bookingConfigApi.reducer,
     bookingTimeline: bookingTimelineReducer,
+    personalDetails: personalDetailsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(bookingConfigApi.middleware),
---
+++ frontend/assignments/2/src/features/personalDetails/personalDetailsSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { PersonalDetails } from "../../types/PersonalDetails";
+
+interface PersonalDetailsState {
+  details: PersonalDetails;
+}
+
+const initialState: PersonalDetailsState = {
+  details: {
+    email: "",
+    phone: "",
+    address: "",
+    pincode: 0,
+  },
+};
+
+const personalDetailsSlice = createSlice({
+  name: "bookingTimeline",
+  initialState,
+  reducers: {
+    setPersonalDetails: (state, action: PayloadAction<PersonalDetails>) => {
+      state.details = action.payload;
+    },
+  },
+});
+
+export const { setPersonalDetails } = personalDetailsSlice.actions;
+export default personalDetailsSlice.reducer;
